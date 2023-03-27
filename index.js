a1bb7add9cf15a97de88882c5f9c6d2ef9a2faad
Joe <joedeola@gmail.com>
2023-03-27 12:27:46 +0100

    Form

---
+++ index.js
@@ -0,0 +1,10 @@
+
+const username = document.getElementById('name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault()
+})
